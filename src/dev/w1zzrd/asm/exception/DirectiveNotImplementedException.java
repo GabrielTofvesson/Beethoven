247d9c53fa577a6cf5912c37b7a517c41bd7afa5
Gabriel Tofvesson <contact@w1zzrd.dev>
2021-01-28 05:50:49 +0100

    Finalize specific exceptions

--- src/dev/w1zzrd/asm/exception/DirectiveNotImplementedException.java
+++ src/dev/w1zzrd/asm/exception/DirectiveNotImplementedException.java
@@ -1,6 +1,6 @@
 package dev.w1zzrd.asm.exception;
 
-public class DirectiveNotImplementedException extends RuntimeException {
+public final class DirectiveNotImplementedException extends RuntimeException {
     public DirectiveNotImplementedException(String directiveName) {
         super("Operation not implemented: "+directiveName);
     }
